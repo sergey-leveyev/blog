6c147dfd866bf80be90fb3b6c26d8c1b7b8f478c
unknown <sargey7@gmail.com>
2021-08-13 15:54:10 +0300

    130821

---
+++ app/database/db.php
@@ -0,0 +1,29 @@
+<?php
+
+require('connect.php');
+
+function tt($value) {
+    echo '<pre>';
+    print_r($value);
+    echo '</pre>';
+}
+
+function selectAll($table) {
+    global $pdo;
+
+    $sql = "SELECT * FROM $table";
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    $errInfo = $query->errorInfo();
+
+    if($errInfo[0] !== PDO::ERR_NONE) {
+        echo $errInfo[2];
+        exit();
+    }
+
+    return $query->fetchAll();
+}
+
+tt(selectAll('users'));
+
+
